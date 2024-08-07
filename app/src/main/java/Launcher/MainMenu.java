3e43b7ee42741be6e3370289d34bf91b46e0ab7e
koro <koro.dev404@gmail.com>
2024-08-07 16:15:03 +0200

    made menu larger

--- app/src/main/java/Launcher/MainMenu.java
+++ app/src/main/java/Launcher/MainMenu.java
@@ -30,10 +30,9 @@ import Window.Window;
 
 public class MainMenu extends JPanel implements ActionListener {
 
-    // TODO: scale everything to make it bigger!!!
     private static JFrame menuFrame;
-    private final int MENU_WIDTH = 800;
-    private final int MENU_HEIGHT = 600;
+    private final int MENU_WIDTH = 1200;
+    private final int MENU_HEIGHT = 900;
 
     int WIDTH;
     int HEIGHT;
@@ -42,30 +41,24 @@ public class MainMenu extends JPanel implements ActionListener {
     Font MINECRAFT_FONT;
 
     // menu element resolutions
-    final int logoWidth = 666;
-    final int logoHeight = 116;
+    final int logoWidth = 999;
+    final int logoHeight = 174;
 
     final int playButtonWidth = 192;
     final int playbuttonHeight = 96;
-
-    // modifiers
-    // ValueModifier seedSizeModifier;
-    // ValueModifier colsModifier;
-    // ValueModifier fpsModifier;
-    // ValueModifier tileSizeModifier;
+    Button playButton;
 
     // modifiable in the menu
-    private MutableInteger SEED = new MutableInteger(42, 1);
-    private MutableInteger COLS = new MutableInteger(8, 1);
-    private MutableInteger ROWS = new MutableInteger(6, 1);
+    private MutableInteger SEED = new MutableInteger(42, 1, 0);
+    private MutableInteger COLS = new MutableInteger(8, 1, 1);
+    private MutableInteger ROWS = new MutableInteger(6, 1, 1);
+    private MutableInteger GRIDOFFSET = new MutableInteger(1, 1, 1);
 
-    private MutableInteger FPS = new MutableInteger(30, 15);
-    private MutableInteger CHUNK_SIZE = new MutableInteger(32, 8);
-    private MutableInteger TILE_SIZE = new MutableInteger(5, 1);
+    private MutableInteger FPS = new MutableInteger(30, 15, 1);
+    private MutableInteger CHUNK_SIZE = new MutableInteger(32, 8, 1);
+    private MutableInteger TILE_SIZE = new MutableInteger(5, 1, 1);
 
 
-
-    Button playButton;
 
 
     public void calculateResolution() {
@@ -119,15 +112,18 @@ public class MainMenu extends JPanel implements ActionListener {
         add(logo);
 
         // modifiers
-        int leftColTitleX = 50;
-        ValueModifier seedSizeModifier = new ValueModifier(leftColTitleX, "seed.png", 120 + leftColTitleX, 210, SEED, this);
-        ValueModifier colsModifier = new ValueModifier(leftColTitleX, "cols.png", 120 + leftColTitleX, 280, COLS, this);
-        ValueModifier rowsModifier = new ValueModifier(leftColTitleX, "rows.png", 120 + leftColTitleX, 350, ROWS, this);
+        int leftColTitleX = 0;
+        int startingY = 270;
+        int verticalOffset = 110;
+        ValueModifier seedSizeModifier = new ValueModifier(leftColTitleX, "seed.png", 240 + leftColTitleX, startingY, SEED, this);
+        ValueModifier colsModifier = new ValueModifier(leftColTitleX, "cols.png", 240 + leftColTitleX, verticalOffset + startingY, COLS, this);
+        ValueModifier rowsModifier = new ValueModifier(leftColTitleX, "rows.png", 240 + leftColTitleX, verticalOffset * 2 + startingY, ROWS, this);
+        ValueModifier gridOffsetModifier = new ValueModifier(leftColTitleX, "grid-offset.png", 240 + leftColTitleX, verticalOffset * 3 + startingY, GRIDOFFSET, this);
 
-        int rightColTitleX = 630;
-        ValueModifier fpsModifier = new ValueModifier(rightColTitleX, "fps.png", MENU_WIDTH / 2, 210, FPS, this);
-        ValueModifier chunkSizeModifier = new ValueModifier(rightColTitleX, "chunk-size.png", MENU_WIDTH / 2, 280, CHUNK_SIZE, this);
-        ValueModifier tileSizeModifier = new ValueModifier(rightColTitleX, "tile-size.png", MENU_WIDTH / 2, 350, TILE_SIZE, this);
+        int rightColTitleX = 950;
+        ValueModifier fpsModifier = new ValueModifier(rightColTitleX, "fps.png", MENU_WIDTH / 2, startingY, FPS, this);
+        ValueModifier chunkSizeModifier = new ValueModifier(rightColTitleX, "chunk-size.png", MENU_WIDTH / 2, verticalOffset + startingY, CHUNK_SIZE, this);
+        ValueModifier tileSizeModifier = new ValueModifier(rightColTitleX, "tile-size.png", MENU_WIDTH / 2, verticalOffset * 2 + startingY, TILE_SIZE, this);
 
         // load play button
         playButton = new Button(centerElemOnX(playButtonWidth), MENU_HEIGHT - playbuttonHeight - 40, playButtonWidth, playbuttonHeight, "menu/play-btn");
@@ -195,24 +191,30 @@ public class MainMenu extends JPanel implements ActionListener {
 }
 
 class MutableInteger {
-    private int val;
+    private final int MAX = 999;
+    private final int MIN;
+
+    private int VAL;
     private int step;
 
     // step, how much to incr or decr
-    public MutableInteger(int val, int step) {
-        this.val = val;
+    public MutableInteger(int val, int step, int min) {
+        this.MIN = min;
+
+        this.VAL = val;
         this.step = step;
     }
     public void incr() {
-        val += step;
+        if (VAL + step > MAX) return;
+        VAL += step;
     }
     public void decr() {
-        if (val - step < 0) return;
-
-        val -= step;
+        if (VAL - step < 0) return;
+        if (VAL - step < MIN) return;
+        VAL -= step;
     }
     public int get() {
-        return val;
+        return VAL;
     }
 }
 
@@ -220,7 +222,7 @@ class MutableInteger {
 class Button extends JButton {
     public String ICON;
     public boolean isPressed;
-    public int milliesTillReleased = 300;
+    public int milliesTillReleased = 200;
 
     public Button(int x, int y, int width, int height, String icon) {
         this.ICON = icon;
@@ -267,8 +269,8 @@ class Button extends JButton {
 class ValueModifier {
     private Button incr;
     private Button decr;
-    private final int ICON_SIZE = 64;
-    private final int LABEL_SIZE_WIDTH = 96;
+    private final int ICON_SIZE = 96;
+    private final int LABEL_SIZE_WIDTH = 144;
 
     private NumberLabel numberLabel;
 
@@ -277,7 +279,7 @@ class ValueModifier {
 
         JLabel tileSizeLabel = new JLabel();
         tileSizeLabel.setIcon(new ImageIcon(Constants.IMAGE_ASSET_DIR + "menu/labels/" + titleFileName));
-        tileSizeLabel.setBounds(titleSizeX, topLeftY + 16, 112, 32);
+        tileSizeLabel.setBounds(titleSizeX, topLeftY + 24, 224, 64);
         menu.add(tileSizeLabel);
 
         incr = new Button(topLeftX, topLeftY, ICON_SIZE, ICON_SIZE, "menu/increment-btn");
@@ -293,7 +295,7 @@ class ValueModifier {
         numberLabel = new NumberLabel(topLeftX + ICON_SIZE, topLeftY, val.get(), menu);
 
         JLabel numLabelPlaque = new JLabel();
-        numLabelPlaque.setIcon(new ImageIcon(Constants.IMAGE_ASSET_DIR + "menu/number-label.png"));
+        numLabelPlaque.setIcon(new ImageIcon(Constants.IMAGE_ASSET_DIR + "menu/number-label" + Constants.IMG_EXT));
         numLabelPlaque.setBounds(topLeftX + ICON_SIZE, topLeftY, LABEL_SIZE_WIDTH, ICON_SIZE);
         menu.add(numLabelPlaque);
 
@@ -319,8 +321,8 @@ class NumberLabel {
     private String num;
 
     // res of each image
-    private final int WIDTH = 20;
-    private final int HEIGHT = 32;
+    private final int WIDTH = 40;
+    private final int HEIGHT = 64;
 
     public NumberLabel(int topLeftX, int topLeftY, int number, MainMenu menu) {
 
@@ -328,10 +330,9 @@ class NumberLabel {
 
         for (int i = 0; i < figures.length; i++) {
             figures[i] = new JLabel();
-            figures[i].setIcon(new ImageIcon(Constants.IMAGE_ASSET_DIR + "menu/nums/" + this.num.charAt(i) + ".png"));
+            figures[i].setIcon(new ImageIcon(Constants.IMAGE_ASSET_DIR + "menu/nums/" + this.num.charAt(i) + Constants.IMG_EXT));
             figures[i].setBounds(topLeftX + WIDTH * i + 15, topLeftY + 14, WIDTH, HEIGHT); // digits are to center it better
             menu.add(figures[i]);
-            System.out.println(Constants.IMAGE_ASSET_DIR + "menu/nums/" + this.num.charAt(i) + ".png");
         }
 
 
@@ -355,7 +356,7 @@ class NumberLabel {
         set(newVal);
 
         for (int i = 0; i < figures.length; i++) {
-            figures[i].setIcon(new ImageIcon(Constants.IMAGE_ASSET_DIR + "menu/nums/" + this.num.charAt(i) + ".png"));
+            figures[i].setIcon(new ImageIcon(Constants.IMAGE_ASSET_DIR + "menu/nums/" + this.num.charAt(i) + Constants.IMG_EXT));
         }
     }
 
--- app/src/main/saves/README.md
+++
@@ -1,7 +0,0 @@
-# Saved word files
-
-unloader saves to disk `here`
-
-folders are `CHUNKSIZE/SEED/*`
-
-the chunks are saved as "y_x.sar" with the top left corner while spawning is 0_0, NOT the viewport, but the starting grid
